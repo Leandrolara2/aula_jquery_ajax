9e349f82e8769aac9c9e3c7012cc5eacd770ac67
laraleandro2 <laraleandro2@gmail.com>
2021-06-09 12:41:30 -0300

    Utilizando Jquery e Ajax - Parte 2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-alert("funcionou");
+function consultaCep(){
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep +"/json/";
+    console.log(url);
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            console.log(response);
+            $("#logradouro").html(response.logradouro);
+            //document.getElementById("logradouro").innerHTML = response.logradouro;
+            document.getElementById("Bairro").innerHTML = response.bairro;
+            $("#localidade").html(response.localidade);
+            document.getElementById("uf").innerHTML = response.uf;
+            //alert(response.logradouro);
+        }
+    })
+}
